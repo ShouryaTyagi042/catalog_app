ce78c7fe48c112ace518132ee3c9d94810940ed6
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-06-26 00:42:00 +0530

    created items_widget file to hold the widget for the item.

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_app/Models/catalog.dart';
 import 'package:flutter_catalog_app/widgets/drawer.dart';
+import 'package:flutter_catalog_app/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const int days = 35 ;
-    const String name = "Shourya" ;
+    final dummyList = List.generate(4, (index) => CatalogModel.items[0]) ;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
-
-      body: Center(
-        child: Container(
-          color: Colors.red,
-          child: const  Text("Welcome to My Flutter App,  $days $name,"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            }),
       ),
       drawer: const MyDrawer(),
     );
