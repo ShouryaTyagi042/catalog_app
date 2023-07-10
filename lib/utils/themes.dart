06bf2db9ac205bcb7d0a555bbbf82b6ccb8a7629
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-10 13:11:31 +0530

    Added the VelocityX package and improved the UI of the application.

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -1,19 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-  primarySwatch: Colors.deepPurple,
-  appBarTheme: const AppBarTheme(
-  color: Colors.white ,
-  elevation: 0.0 ,
-  iconTheme: IconThemeData(color: Colors.black) ,
-  titleTextStyle: TextStyle(color: Colors.black, fontSize: 20)
-  ),
-  fontFamily: GoogleFonts.lato().fontFamily
-  ) ;
-  static ThemeData darkTheme(BuildContext context) => ThemeData(
-      brightness: Brightness.dark
-  ) ;
+      primarySwatch: Colors.deepPurple,
+      appBarTheme: const AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(color: Colors.black, fontSize: 20)),
+      fontFamily: GoogleFonts.poppins().fontFamily);
+
+  static ThemeData darkTheme(BuildContext context) =>
+      ThemeData(brightness: Brightness.dark);
+
+  //Colors
+  static Color creamColor = const Color(0xfff5f5f5);
+
+  static Color darkBluishColor = const Color(0xff403b58);
 }
