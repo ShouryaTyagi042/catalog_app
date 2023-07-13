8713c49845a9794148dc191993ef229a45851a30
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-07-13 20:09:30 +0530

    Created list view in cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -13,7 +13,54 @@ class CartPage extends StatelessWidget {
         centerTitle: true,
       ),
       backgroundColor: MyTheme.creamColor,
-
+      body: Column(
+        children: [
+          const _CartList().p32().expand(),
+          const Divider(),
+          const _CartTotal()
+        ],
+      ),
     );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$999".text.xl4.make(),
+          ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all(const StadiumBorder())),
+              child: "Add to Cart".text.make())
+        ],
+      ),
+    );
+  }
+}
+
+class _CartList extends StatefulWidget {
+  const _CartList();
+
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemCount: 5, itemBuilder: (context,index) => ListTile(
+      leading: const  Icon(Icons.done),
+      trailing: IconButton(icon: const Icon(Icons.remove_circle_outline),
+      onPressed: (){} ,),
+      title: "Item 1".text.make(),
+    ));
   }
 }
